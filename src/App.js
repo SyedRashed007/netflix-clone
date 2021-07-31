1a2e9fdec94c766c41a50035d6f705bb08fdd395
Syed Rasheduddin <SyedRashed007@users.noreply.github.com>
2021-08-01 00:48:53 +0530

    Added Nav

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Row from './components/Row'
 import './requests'
 import requests from './requests';
 import Banner from './components/Banner'
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
+    <Nav/>
     <Banner/>
       <Row title="Netflix Originals"
         fetchUrl={requests.fetchNetflixOriginals}
